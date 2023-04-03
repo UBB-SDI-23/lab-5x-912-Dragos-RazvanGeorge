d3870e32b8da041816b99c679961b3cc44ca02dc
912-Dragos-RazvanGeorge <91673225+912-Dragos-RazvanGeorge@users.noreply.github.com>
2023-04-03 15:17:23 +0300

    Add files via upload

--- lab1/primu/urls.py
+++ lab1/primu/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 from primu import views
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('cars/',views.CarApiView.as_view()),
     path('cars/<int:id>',views.CarDetailView.as_view()),
